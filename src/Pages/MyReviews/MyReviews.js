c9a7c6b8ce7f949503a021fef3d1057d8645ff11
alamin09876 <alamin.ahmed0795@gmail.com>
2022-11-10 04:20:23 +0600

    add course

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 import { Link, useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthProvider/AuthProvider';
 
 const MyReviews = () => {
-    
+
     const [reviews, setReviews] = useState([])
     // const [deleteReview, setDeleteReview] = useState([])
     const { user } = useContext(AuthContext)
@@ -16,23 +17,23 @@ const MyReviews = () => {
     // },[])
 
     // console.log(deleteReview)
-    const handleDelete = review =>{
+    const handleDelete = review => {
         const agree = window.confirm(`Are you sure you want to delete : ${review.reviewData}`)
-        if(agree){
+        if (agree) {
             console.log("Deleting user with id:", review._id)
-            fetch(`http://localhost:5000/reviews/${review._id}`,{
-                method :'DELETE'
+            fetch(`http://localhost:5000/reviews/${review._id}`, {
+                method: 'DELETE'
             })
-            .then(res => res.json())
-            .then(data =>{
-                if(data.deletedCount > 0){
-                    alert("User deleted successfully")
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert("User deleted successfully")
 
-                    const remaining = reviews.filter(dlt => dlt._id !== review._id)
-                    setReviews(remaining)
-                }
-                console.log(data)
-            })
+                        const remaining = reviews.filter(dlt => dlt._id !== review._id)
+                        setReviews(remaining)
+                    }
+                    console.log(data)
+                })
         }
     }
     useEffect(() => {
@@ -50,21 +51,40 @@ const MyReviews = () => {
         <div>
             {
                 user?.uid ?
-                <>
-            {
-                filtered.map(rev => <p rev={rev}>{<p>name: {rev.userName}</p>}{rev.reviewData}
-                <button onClick={() => handleDelete(rev)}>X</button>
-                <Link to ={`/update/${rev._id}`}>
-                            <button>Update</button>
-                        </Link>
-                </p>)
+                    <>
+                    <p className='text-4xl font-semibold my-10 text-center'>My Reviews</p>
+                        {
+                            filtered.map(rev => <div rev={rev}>
+                                <div className="card card-side flex flex-row justify-between mb-5 bg-base-100 shadow-xl lg:mx-80">
+                                    <figure ><img className='w-18 rounded-full ml-4' src={rev.userImage} alt="useImage" /></figure>
+                                    <div className="card-body grid grid-cols-2 items-center">
+                                        <div>
+                                            <p className='text-xl'>{rev.userName}</p>
+                                            <h2 className="">{rev.reviewData}</h2>
+                                        </div>
+
+                                        <div className="card-actions justify-end items-center">
+                                            <button className="btn btn-primary">
+                                                <Link to={`/update/${rev._id}`}>
+                                                    <FaEdit></FaEdit>
+                                                </Link>
+                                            </button>
+                                            <button onClick={() => handleDelete(rev)} className="btn btn-primary">
+                                                <FaTrash></FaTrash>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>)
+                        }
+                    </>
+                    :
+                    <>
+                        <p>No reviews found</p>
+                    </>
             }
-                </>
-                :
-                <>
-                <p>No reviews found</p>
-                </>
-            }
+
 
         </div>
     );
